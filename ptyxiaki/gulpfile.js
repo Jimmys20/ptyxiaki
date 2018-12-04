934b954eea563492ab853c2f8a2e2aa0a964dcd8
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2018-12-04 15:50:41 +0200

    added gulp and jquery

---
+++ ptyxiaki/gulpfile.js
@@ -0,0 +1,19 @@
+﻿const gulp = require('gulp');
+//const del = require('del');
+
+const libraries = [
+  "node_modules/font-awesome/**/*",
+  "node_modules/bootstrap/**/*",
+  "node_modules/jquery/**/*"
+];
+
+gulp.task('default', ['copy']);
+
+gulp.task('copy', /*['clean'],*/ () => {
+  return gulp.src(libraries, { base: 'node_modules' })
+    .pipe(gulp.dest('wwwroot/lib'));
+});
+
+//gulp.task('clean', () => {
+
+//});
