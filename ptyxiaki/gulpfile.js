0b6fa9f9cdfef6998ad3848c4f64c5ecedab2804
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2018-12-04 18:06:35 +0200

    added popper.js and datatables.net, updated layout to use bootstrap 4

--- ptyxiaki/gulpfile.js
+++ ptyxiaki/gulpfile.js
@@ -1,18 +1,22 @@
-﻿const gulp = require('gulp');
+﻿var gulp = require('gulp');
 //const del = require('del');
 
-const libraries = [
+var libraries = [
   "node_modules/font-awesome/**/*",
   "node_modules/bootstrap/**/*",
-  "node_modules/jquery/**/*"
+  "node_modules/jquery/**/*",
+  "node_modules/popper.js/**/*",
+  "node_modules/datatables.net/**/*",
+  "node_modules/datatables.net-bs4/**/*"
 ];
 
-gulp.task('default', ['copy']);
 
-gulp.task('copy', /*['clean'],*/ () => {
+gulp.task('copy', function() {
   return gulp.src(libraries, { base: 'node_modules' })
     .pipe(gulp.dest('wwwroot/lib'));
 });
+
+//gulp.task('default', ['copy']);
 
 //gulp.task('clean', () => {
 
