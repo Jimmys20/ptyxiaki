e39f7e44a96940ba3b0d38906551a640ef021ea2
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2018-12-19 01:53:18 +0200

    added del to use with gulp, updated gulp tasks

--- ptyxiaki/gulpfile.js
+++ ptyxiaki/gulpfile.js
@@ -1,23 +1,20 @@
 ﻿var gulp = require('gulp');
-//const del = require('del');
+var del = require('del');
 
 var libraries = [
-  "node_modules/font-awesome/**/*",
-  "node_modules/bootstrap/**/*",
-  "node_modules/jquery/**/*",
-  "node_modules/popper.js/**/*",
-  "node_modules/datatables.net/**/*",
-  "node_modules/datatables.net-bs4/**/*"
+    'node_modules/font-awesome/**/*',
+    'node_modules/bootstrap/**/*',
+    'node_modules/jquery/**/*',
+    'node_modules/datatables.net/**/*',
+    'node_modules/datatables.net-bs4/**/*'
 ];
 
-
-gulp.task('copy', function() {
-  return gulp.src(libraries, { base: 'node_modules' })
-    .pipe(gulp.dest('wwwroot/lib'));
+gulp.task('clean', function () {
+    return del('wwwroot/lib');
 });
 
-//gulp.task('default', ['copy']);
+gulp.task('copy', function () {
+    return gulp.src(libraries, {base: 'node_modules'}).pipe(gulp.dest('wwwroot/lib'));
+});
 
-//gulp.task('clean', () => {
-
-//});
+gulp.task('default', gulp.series('clean', 'copy'));
