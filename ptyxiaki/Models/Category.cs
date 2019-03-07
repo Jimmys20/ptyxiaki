8509da1d60b08f469757736905443e2325cd2842
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2019-03-07 16:47:23 +0200

    .

---
+++ ptyxiaki/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ptyxiaki.Models
+{
+  public class Category
+  {
+    public int categoryId { get; set; }
+    [Required]
+    [Display(Name = "Τίτλος")]
+    public string title { get; set; }
+    [Display(Name = "Περιγραφή")]
+    public string description { get; set; }
+  }
+}
---
+++ ptyxiaki/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ptyxiaki.Common;
+using ptyxiaki.Data;
+using ptyxiaki.Models;
+
+namespace ptyxiaki.Pages.Categories
+{
+  [Authorize(Policy = Globals.ADMINISTRATOR_POLICY)]
+  public class CreateModel : PageModel
+  {
+    private readonly DepartmentContext context;
+
+    public CreateModel(DepartmentContext context)
+    {
+      this.context = context;
+    }
+
+    public IActionResult OnGet()
+    {
+      return Page();
+    }
+
+    [BindProperty]
+    public Category category { get; set; }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      if (!ModelState.IsValid)
+      {
+        return Page();
+      }
+
+      context.categories.Add(category);
+      await context.SaveChangesAsync();
+
+      return RedirectToPage("./Index");
+    }
+  }
+}
