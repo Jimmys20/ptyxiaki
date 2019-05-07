571b86544ac7508f03e93e494216f2ee6235ccb4
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2019-05-08 01:56:54 +0300

    students

--- ptyxiaki/Controllers/ApiController.cs
+++ ptyxiaki/Controllers/ApiController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -16,10 +18,12 @@ namespace ptyxiaki.Controllers
   public class ApiController : Controller
   {
     private readonly DepartmentContext context;
+    private readonly IMapper mapper;
 
-    public ApiController(DepartmentContext context)
+    public ApiController(DepartmentContext context, IMapper mapper)
     {
       this.context = context;
+      this.mapper = mapper;
     }
 
     public class DataTablesParameters
@@ -55,40 +59,40 @@ namespace ptyxiaki.Controllers
 
     [HttpPost]
     [Authorize(Policy = Globals.PROFESSOR_POLICY)]
-    public JsonResult Students(DataTablesParameters parameters)
+    public async Task<JsonResult> students(DataTablesParameters parameters)
     {
-      var data = context.students.AsQueryable();
+      var queryable = context.students.AsQueryable();
 
-      var recordsTotal = data.Count();
+      var recordsTotal = await queryable.CountAsync();
 
       if (parameters.search != null && !string.IsNullOrEmpty(parameters.search.value))
-        data = data.Where(s => s.lastName.ToUpper().Contains(parameters.search.value.ToUpper()) ||
-                               s.firstName.ToUpper().Contains(parameters.search.value.ToUpper()) ||
-                               s.registrationNumber.Contains(parameters.search.value));
-
-      var recordsFiltered = data.Count();
-
-      if (parameters.order != null)
       {
-        var name = parameters.columns[parameters.order[0].column].data;
+        var values = parameters.search.value.Split(' ');
 
-        if (parameters.order[0].dir == "asc")
+        foreach (var value in values)
         {
-          data = data.OrderBy(s => s.GetType().GetProperty(name).GetValue(s, null));
-        }
-        else
-        {
-          data = data.OrderByDescending(s => s.GetType().GetProperty(name).GetValue(s, null));
+          queryable = queryable.Where(s => EF.Functions.ILike(s.lastName, $"%{value}%") ||
+                                           EF.Functions.ILike(s.firstName, $"%{value}%") ||
+                                           EF.Functions.ILike(s.registrationNumber, $"%{value}%"));
         }
       }
 
+      var recordsFiltered = await queryable.CountAsync();
+
+      if (parameters.order != null)
+      {
+        var orders = parameters.order.Select(o => $"{parameters.columns[o.column].data} {o.dir}").ToList();
+        var ordering = string.Join(", ", orders);
+        queryable = queryable.OrderBy(ordering);
+      }
+
       if (parameters.start != null)
-        data = data.Skip(parameters.start.Value);
+        queryable = queryable.Skip(parameters.start.Value);
 
       if (parameters.length != null)
-        data = data.Take(parameters.length.Value);
+        queryable = queryable.Take(parameters.length.Value);
 
-      data.ToList();
+      var data = await queryable.ToListAsync();
 
       return Json(new
       {
