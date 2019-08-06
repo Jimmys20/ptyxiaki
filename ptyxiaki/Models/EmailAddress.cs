98afaef0fadf2346ae093ab1ff57af556f30a69b
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2019-08-06 15:57:21 +0300

    email address model

---
+++ ptyxiaki/Models/EmailAddress.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ptyxiaki.Models
+{
+  public class EmailAddress
+  {
+    public string name { get; set; }
+    public string address { get; set; }
+
+    public EmailAddress()
+    {
+    }
+
+    public EmailAddress(string name, string address)
+    {
+      this.name = name;
+      this.address = address;
+    }
+  }
+}
