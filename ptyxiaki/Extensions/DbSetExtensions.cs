08f306496dbb57ffd6cd86341d63e63a38c227de
Jimmys20 <34100964+Jimmys20@users.noreply.github.com>
2019-08-09 13:09:51 +0300

    DbSetExtensions

---
+++ ptyxiaki/Extensions/DbSetExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ptyxiaki.Common;
+using ptyxiaki.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ptyxiaki.Extensions
+{
+  public static class DbSetExtensions
+  {
+    public static async Task<int?> getCurrentSemesterIdAsync(this DbSet<Semester> semesters)
+    {
+      var semester = await semesters.OrderBy(s => s.createdAt).FirstOrDefaultAsync();
+      return semester?.semesterId;
+    }
+
+    public static bool isPostPeriod(this DbSet<Date> dates)
+    {
+      var date = dates.FirstOrDefault();
+      var now = DateTime.Now;
+
+      if (date != null &&
+          date.postStart.HasValue && now >= date.postStart.Value &&
+          date.postEnd.HasValue && now <= date.postEnd.Value)
+        return true;
+
+      return false;
+    }
+
+    public static bool isDeclarationPeriod(this DbSet<Date> dates)
+    {
+      var date = dates.FirstOrDefault();
+      var now = DateTime.Now;
+
+      if (date != null &&
+          date.declarationStart.HasValue && now >= date.declarationStart.Value &&
+          date.declarationEnd.HasValue && now <= date.declarationEnd.Value)
+        return true;
+
+      return false;
+    }
+
+    public static IQueryable<Student> getStudentsWhoMeetRequirements(this DbSet<Student> students)
+    {
+      return students.Where(s => s.semester >= Globals.STUDENT_SEMESTER_REQUIREMENT &&
+                                 s.credits >= Globals.STUDENT_CREDITS_REQUIREMENT &&
+                                 !s.assignments.Any(a => a.thesis.status != Status.Canceled));
+    }
+  }
+}
